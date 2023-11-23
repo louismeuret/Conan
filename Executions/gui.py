62ec4f8d44dfbbbf908a8863db3d800495c7f3a9
Louis <louis23012001@gmail.com>
2023-11-23 02:25:11 +0100

    Fix settings edit

--- Executions/gui.py
+++ Executions/gui.py
@@ -11,7 +11,7 @@ from subprocess import Popen, PIPE
 dpg.create_context()
 dpg.create_viewport(title="Conan", width=800, height=500)
 
-dpg.show_metrics()
+#dpg.show_metrics()
 
 
 def boutonsave(sender, data):
@@ -453,9 +453,10 @@ def windowsettingsopen(path):
         dpg.add_input_text(tag="", label="test2", multiline=True, height=500, width=800, default_value=block_config)
 
 def settingseditor(sender, data):
-    soft = dpg.get_value("Softfock")
+    soft = dpg.get_value("Softdock")
+    # need to 
     if soft == "Autodock-vina" or "Smina" or "Qvina":
-        windowsettingsopen("/home/louis/Conan/newparametres/DOCKING.dpf")
+        windowsettingsopen("/home/louis/Conan/parametres/DOCKING.dpf")
 
 
 def view_grid(sender, data):
